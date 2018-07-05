8e1cda8aa016fa5cbc2a28dab6ebfaaf918b2803
Daniel Owen <owend@couchbase.com>
2018-07-05 19:43:34 +0100

    initial example

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=gymexample-Web-Deploy.pubxml -p:Password=3D4J2DJxrJvqpB1bbB9f3tunw8DvrFlKEz9ElXQwb7ALxNxQFKYLTvAL1p4p
+
